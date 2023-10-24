0161750585ba3732e4f872559be48f8bd576b779
GevinN99 <gevinnanayakkara@gmail.com>
2023-10-24 13:34:20 +0530

    Frontend WelcomePage, LoginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import HomePage from "./components/HomePage";
+import WelcomePage from "./components/WelcomePage";
+import SignupPage from "./components/SignupPage";
 
 function App() {
   return (
       <BrowserRouter>
         <Routes>
-            <Route path="/" element={<LoginPage/>}/>
+            <Route path="/login" element={<LoginPage/>}/>
+            <Route path="/signup" element={<SignupPage/>}/>
             <Route path="/home" element={<HomePage/>}/>
+            <Route path="/" element={<WelcomePage/>}/>
         </Routes>
       </BrowserRouter>
   );
